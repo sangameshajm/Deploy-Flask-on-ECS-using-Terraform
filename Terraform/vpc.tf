7fa4b071b21d3870b1c6b10739da5dd5c25abcdd
Sangam <33129198+sangameshajm@users.noreply.github.com>
2021-03-25 13:54:44 +0000

    Update vpc.tf

--- Terraform/vpc.tf
+++ Terraform/vpc.tf
@@ -70,5 +70,19 @@ resource "aws_security_group" "ecs_assignment" {
         protocol        = "tcp"
         cidr_blocks     = ["0.0.0.0/0"]
     }
+    
+    ingress {
+        from_port       = 22
+        to_port         = 22
+        protocol        = "tcp"
+        cidr_blocks     = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port       = 0
+        to_port         = 65535
+        protocol        = "tcp"
+        cidr_blocks     = ["0.0.0.0/0"]
+    }
 
 }
